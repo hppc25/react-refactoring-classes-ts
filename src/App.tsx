3f6fe97babc53a9d3d7403b874e86f700737fed6
Helder Correia <helder.patrick25@gmail.com>
2022-05-11 00:01:01 +0100

    migration to Typescript and Function Components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
 
 import GlobalStyle from './styles/global';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 const App = () => (
   <>
